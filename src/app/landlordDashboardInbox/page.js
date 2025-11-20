920c981c210bf4a5777ad824a422d543bf74f2dd
sennyrulz <oloto.oluwaseun@gmail.com>
2025-11-20 15:55:54 +0100

    workingonlandlordmodal

--- src/app/landlordDashboardInbox/page.js
+++ src/app/landlordDashboardInbox/page.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import Link from 'next/link'
-import LandlordDashboardSidebar from '../../components/landlordDashboardSidebar/index.js'
-import LandlordDashboardFooter from '../../components/landlordDashboardFooter/index.js'
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import LandlordDashboardSidebar from '../../components/landlordDashboardSidebar/index.js';
+import LandlordDashboardFooter from '../../components/landlordDashboardFooter/index.js';
+import SubscriptionModal1 from '../../components/subscriptionModal1/index.js';
 
 const page = () => {
+  const [openModal, setOpenModal] = useState(false);
   return (
       <div>
           <LandlordDashboardSidebar />
-          <div className='landlordDashboardInboxContainer'>
-            <div className="landlordInbox mt-8 p-6 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.1)">
-              <h3 className="text-4xl text-blue-800 font-bold">Inbox</h3>
-            </div>
-        
-            <div className="landlordinbox mt-8 p-6 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.1)">
-              <h5 className="text-xl font-semibold"> my messages</h5>
-                    <Link href="/">
-                        <p>compose</p>
-                    </Link>
+          <div className="landlordDashboardInboxContainer">
+              <div className="landlordInbox mt-8 p-6 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.1)">
+                  <h3 className="text-4xl text-blue-800 font-bold">Inbox</h3>
+              </div>
+
+              <div className="mt-8 p-6 bg-white shadow-[0_4px_6px_rgba(0,0,0,0.1) flex justify-between">
+                  <h5 className="text-xl font-semibold"> my messages</h5>
                   <Link href="/">
-                      <button className="cursor-pointer">show profile</button>
+                      <p>compose</p>
                   </Link>
+                  <img src="/profile_Pic.png" alt="demo" />
+                  <button className="cursor-pointer text-white bg-blue-800 p-4"
+                              onClick={() => setOpenModal(true)}>
+                                show profile
+                    </button>
               </div>
           </div>
-          <LandlordDashboardFooter />
+      <LandlordDashboardFooter />
+      {/* MODAL*/}
+      <SubscriptionModal1 isOpen={openModal} onClose={() => setOpenModal(false)}  />
       </div>
   );
 }
---
+++ src/components/subscriptionModal1/index.js
@@ -0,0 +1,35 @@
+
+"use client";
+
+import React from 'react'
+
+const index = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+    
+  return (
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-stone-900 w-[275px] h-[394px]  flex items-center justify-center z-50">
+          <div className=" ">
+              <div className="rounded-lg">
+                  <button
+                      className="absolute top-2 right-2 text-xl bg-white "
+                      onClick={onClose}>
+                      x
+                  </button>
+              </div>
+
+              <div className="text-center text-white">
+                  <div className="text-red-600 bg-white rounded-full w-40 h-40">x</div>
+                  <h2>Error</h2>
+                  <p>You are on a free subscription</p>
+                  <p>
+                      please upgrade your plan to a premium plan to view your
+                      profile
+                  </p>
+                  <button>Subscibe Now</button>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default index
