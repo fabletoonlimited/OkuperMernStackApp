31d1fbb3df51d3ceb0a57903ddd3703e8c60489d
sennyrulz <oloto.oluwaseun@gmail.com>
2026-01-05 18:21:29 +0100

    updated property Card Expanded

--- src/app/propertyCardExpanded/page.js
+++ src/app/propertyCardExpanded/page.js
@@ -1,19 +1,92 @@
-import React from 'react'
-import PropExpandedNav from '../../components/propExpandedNav/index.js'
+import React from "react";
+import PropExpandedNav from "../../components/propExpandedNav/index.js";
 
 const index = () => {
-  return (
-    <div>
-      <PropExpandedNav />
-        <div className='property-card-expanded'>
-            <h1>Property Card Expanded</h1>
-            <p>
-            This is the expanded view of a property card. It provides detailed information about the property,
-            including features, amenities, and other relevant details that help potential buyers or renters make informed decisions.
-            </p>    
-        </div>
-    </div>
-  )
-}
+    return (
+        <div>
+            <PropExpandedNav />
+            <div className="property-card-expanded">
+                <div className="grid grid-cols-1 md:grid-cols-4 md:grid-rows-2 gap-2">
+                    <div className="md:col-span-2 md:row-span-2 w-full h-[56rem] md:h-auto overflow-hidden">
+                        <img
+                            src="/property-image.jpg"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
 
-export default index
+                    <div className="w-full h-28 md:h-40 overflow-hidden">
+                        <img
+                            src="/property-image.jpg"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+
+                    <div className="w-full h-28 md:h-40 overflow-hidden">
+                        <img
+                            src="/property-image.jpg"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+
+                    <div className="w-full h-28 md:h-40 overflow-hidden">
+                        <img
+                            src="/property-image.jpg"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+
+                    <div className="w-full h-28 md:h-40 overflow-hidden">
+                        <img
+                            src="/property-image.jpg"
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                </div>
+                < div className="flex justify-around">
+                    <div className="mt-6 p-4  rounded-lg  w-full">
+                        <h2 className="font-bold text-5xl mb-1">
+                            Four Bedroom Flat with BQ
+                        </h2>
+                        <h4 className="text-gray-600 mb-2 text-2xl">
+                            Adelabu, Surulere, Lagos
+                        </h4>
+
+                        <div className="">
+                            <span className="text-5xl font-extrabold mb-1">
+                                ₦5,500,000
+                            </span>
+                            <h6 className="text-gray-500 mb-2 text-2xl">
+                                Price is base rent and doesn't require fees
+                            </h6>
+                        </div>
+
+                        <div className="flex gap-2">
+                            <button className="px-3 py-1 bg-gray-100 text-2xl border-black border-2">
+                                Bungalow building
+                            </button>
+                            <button className="px-3 py-1 bg-gray-100 text-2xl border-black border-2">
+                                3 Bed | 2 Toilet
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <button className="bg-blue-950">
+                            Request to apply
+                        </button>
+                        <br />
+                        <h5>Rate</h5>
+                        <br />
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default index;
--- src/components/propExpandedNav/index.js
+++ src/components/propExpandedNav/index.js
@@ -8,17 +8,16 @@ const index = () => {
     const router = useRouter();
 
     return (
-        <div className="exp-prop-nav bg-white flex items-center justify-between px-4 py-3">
-            <div className="flex items-center back-to-listing gap-3 cursor-pointer">
-                <FaAngleLeft 
-                    className="text-blue-800"
-                />
-                <Link href="/rent">
-                    <h3 
-                    className="font-regular text-blue-800"
-                    >Back to Listing</h3>
-                </Link>
-            </div>
+        <div>
+            <div className="exp-prop-nav bg-white flex items-center justify-between px-4 py-3">
+                <div className="flex items-center back-to-listing gap-3 cursor-pointer">
+                    <FaAngleLeft className="text-blue-800" />
+                    <Link href="/rent">
+                        <h3 className="font-regular text-blue-800">
+                            Back to Listing
+                        </h3>
+                    </Link>
+                </div>
                 <Link href="/">
                     <div className="flex items-center ml-26">
                         <img
@@ -29,36 +28,37 @@ const index = () => {
                     </div>
                 </Link>
 
-            <div className="flex items-center justify-around gap-10 mr-18">
-                <Link href="/savedHomes">
-                    <button className="w-14 h-14">
-                        <img
-                            src="/Save_House_Icon.png"
-                            alt="Save Icon"
-                            className="w-full h-full object-contain"
-                        />
-                    </button>
-                </Link>
+                <div className="flex items-center justify-around gap-10 mr-18">
+                    <Link href="/savedHomes">
+                        <button className="w-14 h-14">
+                            <img
+                                src="/Save_House_Icon.png"
+                                alt="Save Icon"
+                                className="w-full h-full object-contain"
+                            />
+                        </button>
+                    </Link>
 
-                <Link href="/share">
-                    <button className="w-14 h-14">
-                        <img
-                            src="/Share_Icon.png"
-                            alt="Share Icon"
-                            className="w-full h-full object-contain"
-                        />
-                    </button>
-                </Link>
+                    <Link href="/share">
+                        <button className="w-14 h-14">
+                            <img
+                                src="/Share_Icon.png"
+                                alt="Share Icon"
+                                className="w-full h-full object-contain"
+                            />
+                        </button>
+                    </Link>
 
-                <Link href="/report">
-                    <button className="w-14 h-14">
-                        <img
-                            src="/Report_Icon.png"
-                            alt="Report Icon"
-                            className="w-full h-full object-contain"
-                        />
-                    </button>
-                </Link>
+                    <Link href="/Contact">
+                        <button className="w-14 h-14">
+                            <img
+                                src="/Report_Icon.png"
+                                alt="Report Icon"
+                                className="w-full h-full object-contain"
+                            />
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
